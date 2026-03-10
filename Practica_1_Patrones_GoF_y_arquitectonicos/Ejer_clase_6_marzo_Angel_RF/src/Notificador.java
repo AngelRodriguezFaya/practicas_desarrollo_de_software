b38e166c936511a3517c42b3c16ce2429c2c0367
Ángel Rodríguez Faya <147878174+AngelRodriguezFaya@users.noreply.github.com>
2026-03-10 18:05:45 +0100

    Se ha borrado un directorio

--- Practica_1_Patrones_GoF_y_arquitectonicos/Ejer_clase_6_marzo_Angel_RF/src/Notificador.java
+++
@@ -1,3 +0,0 @@
-public interface Notificador {
-    String enviarMensaje();
-}
--- Practica_1_Patrones_GoF_y_arquitectonicos/Ejer_clase_6_marzo_Angel_RF/src/NotificadorSlack.java
+++
@@ -1,9 +0,0 @@
-public class NotificadorSlack extends NotificadorOtro{
-    public NotificadorSlack(Notificador notificador) {
-        super(notificador);
-    }
-        @Override
-        public String enviarMensaje() {
-            return "Enviando SMS: " + super.enviarMensaje();
-        }
-}
